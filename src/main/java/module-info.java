d199a53455416c08b3ef4c03f8e24577ada87e80
gual14 <sobol.gal@gmail.com>
2024-07-06 01:35:55 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.galmusicplayer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.galmusicplayer to javafx.fxml;
+    exports com.example.galmusicplayer;
+}
