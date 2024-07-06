d542ba0458401a5b9f6faf14e04828a7b8a331fe
gual14 <sobol.gal@gmail.com>
2024-07-06 09:04:18 +0300

    changed all default file names

--- src/main/java/com/example/galmusicplayer/AppMain.java
+++ src/main/java/com/example/galmusicplayer/AppMain.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AppMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("player-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gal Music Player");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/galmusicplayer/PlayerController.java
+++ src/main/java/com/example/galmusicplayer/PlayerController.java
@@ -3,7 +3,7 @@ package com.example.galmusicplayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class PlayerController {
     @FXML
     private Label welcomeText;
 
