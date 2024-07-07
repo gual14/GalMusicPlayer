17f86a09246e7c820eb771e4abf3c2c39adaa6cd
gual14 <sobol.gal@gmail.com>
2024-07-07 23:15:56 +0300

    added software icon

--- src/main/java/com/example/galmusicplayer/AppMain.java
+++ src/main/java/com/example/galmusicplayer/AppMain.java
@@ -3,6 +3,7 @@ package com.example.galmusicplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
@@ -13,11 +14,13 @@ public class AppMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("player-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 300);
-        //stage.setResizable(false);
+        stage.setResizable(false);
         stage.setTitle("Gal Music Player");
         PlayerController controller = fxmlLoader.getController();
         scene.addEventFilter(KeyEvent.KEY_PRESSED, controller::handleKeyPressed);
         stage.setScene(scene);
+        Image image = new Image("C:\\Users\\User\\IdeaProjects\\GalMusicPlayer\\src\\main\\resources\\com\\example\\galmusicplayer\\sound-waves.png");
+        stage.getIcons().add(image);
         stage.show();
     }
 
